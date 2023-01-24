96318293cb9ac897f46595626e7ee73a664d27ff
Serafim <serafimkremnev1008@gmail.com>
2023-01-24 19:35:33 +0300

    app fix

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import TaskList from "./components/TaskList/TaskList";
 import {Routes, Route, Link, RouterProvider, useLocation} from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import router from "./router";
+import React from "react";
 
 function App() {
+
   return (
     <div className="App">
       <RouterProvider router={router} />
