72ce6d20d89fe57d95009d8bb33ba72f0179a603
Serafim <serafimkremnev1008@gmail.com>
2023-01-25 10:02:04 +0300

    adaptive

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import styles from './Header.module.css'
 import cn from "classnames";
 
-const Header = ({className}) => {
+const Header = ({className, activeSidebar, setActiveSidebar}) => {
+
   return (
     <header className={cn(styles.header, className)}>
-      To Do
+      <div className={styles.title}>To Do</div><div onClick={()=>setActiveSidebar(()=>!activeSidebar)} className={cn(styles.burger, {
+        [styles.burgerClose]: activeSidebar
+    })}></div>
     </header>
   );
 };
--- src/components/Layout/Sidebar/Sidebar.jsx
+++ src/components/Layout/Sidebar/Sidebar.jsx
@@ -4,15 +4,18 @@ import cn from 'classnames'
 import {NavLink} from "react-router-dom";
 import Groups from "../Groups/Groups";
 
-const Sidebar = ({className}) => {
+const Sidebar = ({className,setActiveSidebar}) => {
   const [openGroup, setOpenGroup] = useState(true)
+  function close() {
+    setActiveSidebar(false)
+  }
   return (
     <div className={cn(styles.sidebar, className)}>
-      <NavLink className={styles.sidebarLink} to={'/create'}>Создать задачу</NavLink>
-      <NavLink className={styles.sidebarLink} to={'/tasks'}>Все задачи</NavLink>
-      <NavLink className={styles.sidebarLink} to={'/today'}>Сегодня</NavLink>
-      <NavLink className={styles.sidebarLink} to={'/important'}>Важно</NavLink>
-      <NavLink className={styles.sidebarLink} to={'/complete'}>Завершенные</NavLink>
+      <NavLink onClick={close} className={styles.sidebarLink} to={'/create'}>Создать задачу</NavLink>
+      <NavLink onClick={close} className={styles.sidebarLink} to={'/tasks'}>Все задачи</NavLink>
+      <NavLink onClick={close} className={styles.sidebarLink} to={'/today'}>Сегодня</NavLink>
+      <NavLink onClick={close} className={styles.sidebarLink} to={'/important'}>Важно</NavLink>
+      <NavLink onClick={close} className={styles.sidebarLink} to={'/complete'}>Завершенные</NavLink>
       <div className={styles.stick}></div>
       <button onClick={()=>setOpenGroup(()=>!openGroup)} className={styles.button}>Группы</button>
       <Groups openGroup={openGroup}/>
